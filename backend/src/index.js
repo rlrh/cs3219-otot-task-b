a2d6bbc5fb7e4a11f6403d401115a9191af1409c
Rayner Lim <rlrh1996@gmail.com>
2020-08-25 23:48:02 +0800

    implement session, user, message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
     req.context = {
         models,
-        me: models.users[1],
+        me: await models.User.findByLogin('rayner.lim'),
     };
     next();
 });
