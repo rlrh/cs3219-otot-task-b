d9e3b16a5614683397af8294b7e25d359af34c20
Rayner Lim <rlrh1996@gmail.com>
2020-08-25 21:48:42 +0800

    setup cors and dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
  
 const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
  
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+    console.log(`Example app listening on port ${process.env.PORT}!`),
 );
