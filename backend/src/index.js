9138b05c91d4edb717848d0d6a974e51937dea1b
Rayner Lim <rlrh1996@gmail.com>
2020-08-25 22:42:02 +0800

    setup models, routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,66 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid';
 import 'dotenv/config';
+import models from './models';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-let users = {
-    1: {
-        id: '1',
-        username: 'Robin Wieruch',
-    },
-    2: {
-        id: '2',
-        username: 'Dave Davids',
-    },
-};
-
-let messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'By World',
-        userId: '2',
-    },
-};
-
-app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
 });
 
-app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
-});
-
-app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user resource');
-});
-
-app.put('/users/:userId', (req, res) => {
-    return res.send(
-        `PUT HTTP method on user/${req.params.userId} resource`,
-    );
-});
-
-app.delete('/users/:userId', (req, res) => {
-    return res.send(
-        `DELETE HTTP method on user/${req.params.userId} resource`,
-    );
-});
-
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
-});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(3000, () =>
     console.log(`Example app listening on port ${process.env.PORT}!`),
