1b247ba623db30e4ace376040ceaf1010fcc75dc
Rayner Lim <rlrh1996@gmail.com>
2020-09-26 00:22:00 +0800

    setup frontend client

---
+++ client/src/components/MessageList.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul>
+        <li v-for="message in messages" :key="message._id">
+            <Message :text="message.text" :user="message.user" :message-id="message._id" />
+        </li>
+    </ul>
+</template>
+
+<script>
+import Message from './Message.vue'
+
+export default {
+  name: 'MessageList',
+  components: {
+    Message
+  },
+  data () {
+    return {
+      messages: null
+    }
+  },
+  mounted () {
+    fetch('https://cs3219-a0180340u-otot-task-b.herokuapp.com/messages')
+    .then(response => response.json())
+    .then(data => this.messages = data)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
