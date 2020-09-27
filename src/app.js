c7dd226b919748eed5f653a7e54c0f1392c8f2cf
Rayner Lim <rlrh1996@gmail.com>
2020-09-27 13:30:03 +0800

    Fix static dirname

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname, '/public/'));
+    app.use(express.static(__dirname + '/public/'));
     app.get("*", (req, res) => {
       res.sendFile(__dirname + '/public/index.html');
     })
